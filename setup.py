88a2da3ed02116f203ab8336d5a09acb5c13f599
Valentin <Valentin_re@web.de>
2025-03-31 11:38:10 +0200

    ready to build package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vallog',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Valentin Reichenspurner',
+    author_email='v.reichenspurner@tum.de',
+    description='simple package for divers and versatile log massages. Future releases will add more functionality.',
+    url='https://github.com/vallereichi/vallog',
+)
